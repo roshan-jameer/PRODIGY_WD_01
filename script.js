fe2c108566e39bc844ad4d0cf3036f3f0a5e4643
roshan-jameer <skroshanjameer325@gmail.com>
2024-09-03 21:41:01 +0530

    commit to main

---
+++ script.js
@@ -0,0 +1,9 @@
+// Change the navbar background color when scrolled
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
